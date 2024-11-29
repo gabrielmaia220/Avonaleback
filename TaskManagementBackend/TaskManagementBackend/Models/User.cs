fd55ec10f60426d0618b75f8d838816274d92e2f
gabrielmaia220 <159305213+gabrielmaia220@users.noreply.github.com>
2024-11-28 21:16:26 -0300

    Delete TaskManagementBackend directory

--- TaskManagementBackend/TaskManagementBackend/Models/User.cs
+++
@@ -1,7 +0,0 @@
-﻿public class User
-{
-    public int Id { get; set; }
-    public string Username { get; set; } = string.Empty;
-    public string PasswordHash { get; set; } = string.Empty;
-    public string Role { get; set; } = "user"; // Valores: admin, user, externo
-}
